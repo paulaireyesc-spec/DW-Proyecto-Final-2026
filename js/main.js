5d2841b33a70a77781e4b98317dc45c860766f2b
paula <paulaireyesc@gmail.com>
2026-04-04 16:48:03 +0100

    inicio-avances
    quite el absolute, mejoré el sticky de proyectos

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// INDEX.HTML - Interacciones
+
+/* => Interacción Sección de proyectos
+      Descripción: Al hacer scroll, los proyectos se acumulan en un mismo punto con un efecto de profundidad
+*/
