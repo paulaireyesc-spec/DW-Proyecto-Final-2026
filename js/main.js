b4d867ab71e572e0034edaef38d1839fad41b33d
paula <paulaireyesc@gmail.com>
2026-04-07 13:16:42 +0100

    services-avances

--- js/main.js
+++ js/main.js
@@ -97,3 +97,11 @@ window.addEventListener('scroll', () => {
       lastScroll = currentScroll;
 });
 }
+
+
+
+// SERVICES.HTML - Interacciones
+
+/* => Interacción A CTA de contacto
+      Descripción: Al hacer scroll, el título de la sección aparece
+*/
