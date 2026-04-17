dd93cc5c7b2e7b562184b235d8b5d8c2733e7ad1
paula <paulaireyesc@gmail.com>
2026-04-17 13:31:11 +0200

    revisiones

--- js/main.js
+++ js/main.js
@@ -1,126 +1,227 @@
-// INDEX.HTML - Interacciones
+/* ---------------------------------------------------------------- 
+      main.js
+      Descripción: Este archivo contiene el código JavaScript para las interacciones de la página web completa.
+      
+      Estructura:
+            - Interacción A: Desvanecimiento del título de sección de proyectos (index.html)
+            - Interacción B: Estilos para la galería sticky de proyectos (index.html)
+            - Interacción C: Menú móvil (mostrar/ocultar menú desplegable en toda la web)
+            - Interacción D: Scroll del título de los banner (en todas las páginas con banner inicial)
+            - Interacción E: Lightbox para las imágenes de los proyectos (en la página de cada proyecto)
+---------------------------------------------------------------- */
 
-/* => Interacción A Sección de proyectos
+
+
+
+/* => Interacción A: Desvanecimiento del título de sección de proyectos
       Descripción: Al hacer scroll, el título de la sección se desvanece
+      Estructura:
+            - Constantes
+            - Función
+            - Evento de scroll
 */
-const projectPreview = document.querySelectorAll('.preview');
-const projectTitle = document.querySelector('.projects__h2');
+const projectPreview = document.querySelectorAll('.preview'); // Selecciona el contendor de cada proyecto.
+const projectTitle = document.querySelector('.projects__h2'); // Selecciona el título de la sección de proyectos.
 
-// Si el array de proyectos no está vacío, añadimos el evento de scroll
-if (projectPreview.length) {
-      window.addEventListener('scroll', () => {
-            // Obtenemos la posición del último proyecto
-            const posicionScroll = projectPreview[3].getBoundingClientRect();
+// Función que desvanece el título de la sección de proyectos
+function opacityHandler() {
+      // Obtenemos la posición del último proyecto
+      const posicionScroll = projectPreview[3].getBoundingClientRect();
 
-            // Si su distancia con el borde superior de la ventana es menor al 100%
-            if (posicionScroll.top < window.innerHeight * 0.1) {
-                  projectTitle.style.opacity = '0'; // Desvanecemos el título
-            } else {
-                  projectTitle.style.opacity = '1';
-            }
-      });
+      // Si su distancia con el borde superior de la ventana es menor al 100%
+      if (posicionScroll.top < window.innerHeight * 0.1) {
+            projectTitle.style.opacity = '0'; // Desvanecemos el título
+      } else {
+            projectTitle.style.opacity = '1'; // Si no, el título vuelve a su estado original
+      }
 }
 
-/* => Interacción B Sección de proyectos
-      Descripción: Al hacer scroll, los proyectos que no tienen el foco cambian de estilo
-      Constantes:
-            - Reciclamos la constante projectPreview del bloque anterior, pero convertida en array
+if (projectPreview.length) { // Si el array de proyectos no está vacío
+      window.addEventListener('scroll', opacityHandler); // Añadimos el evento de scroll con la función de desvanecimiento del título
+}
+
+
+
+
+/* => Interacción B: Estilos para la galería sticky de proyectos
+      Descripción: 
+            1º - Al hacer scroll, los proyectos que no tienen el foco (están detrás) cambian de estilo.
+            2º - El proyecto que tiene el foco se mantiene fijo en la pantalla, mientras los demás se van superponiendo a él.
+
+      Estructura:
+            - Constantes
+            - Función
+            - Eventos de scroll y resize
 
       ACLARACIÓN: Ayuda de ChatGPT para resolver el problema del cálculo de la superposición.
 */
-const previews = [...document.querySelectorAll(".preview")];
 
-if (previews) {
-      // Función para actualizar el estilo de las previews según su posición entre sí
-      function updatePreview() {
-            previews.forEach((preview) => {
-                  // Reseteamos el estado de cada preview
-                  preview.classList.remove("preview--disabled");
-            });
+const previews = document.querySelectorAll(".preview"); // Selecciona todos los contenedores de los proyectos.
 
-            // Bucle para definir el proyecto anterior y actual
-            for (let i = 1; i < previews.length; i++) {
-                  const current = previews[i]; // proyecto actual
-                  const previous = previews[i - 1]; // proyecto anterior
+// Función que actualiza el estilo de los proyectos según su posición en la pantalla.
+function updatePreview() {
+      previews.forEach((preview) => {
+            // Reseteamos el estilo de los contenedores a su estado original (activo)
+            preview.classList.remove("preview--disabled");
+      });
 
-                  const currentRect = current.getBoundingClientRect(); // información del proyecto actual
-                  const previousRect = previous.getBoundingClientRect(); // información del proyecto anterior
+      for (let i = 1; i < previews.length; i++) {
+            const current = previews[i]; // Registramos la posición del proyecto actual
+            const previous = previews[i - 1]; // Y la del proyecto anterior
 
-                  // Cuando la actual invade a la anterior (se superpone)
-                  if (currentRect.top < previousRect.bottom) {
-                        previous.classList.add("preview--disabled"); // Desactivamos el proyecto anterior
-                  }
+            const currentProject = current.getBoundingClientRect(); // Recibimos la información del proyecto actual
+            const previousProject = previous.getBoundingClientRect(); // Recibimos la información del proyecto anterior
+
+            // Cuando el proyecto actual invade al anterior (se superpone)
+            if (currentProject.top < previousProject.bottom) {
+                  previous.classList.add("preview--disabled"); // Desactivamos el proyecto anterior
             }
       }
-
-      window.addEventListener("scroll", updatePreview); // Actualiza el estilo según el scroll
-      window.addEventListener("resize", updatePreview); // Actualiza el estilo según el cambio de tamaño de la ventana
-
-      updatePreview();
 }
 
-/* => Interacción C Menú móvil
-      Descripción: Al hacer click en el icono del menú, se muestra el menú desplegable
-*/
-const menuIcon = document.querySelector('.header__menu-icon');
-const mobileMenu = document.querySelector('.header__menu--mobile');
-const header = document.querySelector('.header');
+if (previews) { // Si existen los contenedores de proyectos
 
-menuIcon.addEventListener('click', () => {
+      window.addEventListener("scroll", updatePreview); // Se actualiza el contenedor según el scroll
+      window.addEventListener("resize", updatePreview); // Actualiza el contenedor según el cambio de tamaño de la ventana
+
+      updatePreview(); // Evitamos efectos raros llamando a la función al cargar la página, para que el estilo se actualice desde el principio.
+}
+
+
+
+
+/* => Interacción C: Menú móvil
+      Descripción: Al hacer click en el icono del menú, se muestra el menú desplegable.
+      Estructura:
+            - Constantes
+            - Función
+            - Evento de click para mostrar/ocultar el menú desplegable
+*/
+const menuIcon = document.querySelector('.header__menu-icon'); // Selecciona el icono del menú
+const mobileMenu = document.querySelector('.header__menu--mobile'); // Selecciona el menú para móviles
+const header = document.querySelector('.header'); // Selecciona el encabezado
+
+// Función para mostrar/ocultar el menú desplegable
+function mobileMenuHandler() {
+      // Si el menú tiene la clase activo
       if (mobileMenu.classList.contains('header__menu--active')) {
+            // Lo reseteamos a su estado original (oculto)
             mobileMenu.classList.remove('header__menu--active');
             header.classList.remove('header--mobile');
       } else {
+            // Si no, añadimos la clase activo para mostrar el menú
             mobileMenu.classList.add('header__menu--active');
             header.classList.add('header--mobile');
       }
-});
+}
+
+// Escuchamos el click en el icono del menú para mostrar/ocultar el menú desplegable
+menuIcon.addEventListener('click', mobileMenuHandler);
 
 
-/* => Interacción D Scroll del título de bienvenida
-      Descripción: Al hacer scroll hacia abajo, el título de bienvenida se mueve hacia la derecha, dejando ver todo el mensaje.
+
+
+/* => Interacción D: Scroll del título de los banners
+      Descripción: Al hacer scroll hacia abajo, el título de los banners iniciales de cada página se mueven a la izquierda.
+      Estructura:
+            - Constantes
+            - Variables
+            - Función
+            - Evento de scroll
 
       ACLARACIÓN: Ayuda de ChatGPT para resolver cómo definir la dirección del scroll.
 */
-const bannerTitle = document.querySelector('.banner__h1');
-let lastScroll = window.scrollY;
+const bannerTitle = document.querySelector('.banner__h1'); // Seleccionamos el título del banner
+let lastScroll = window.scrollY; // Registramos la posición del scroll
+
+// Función para mover el título del banner según la dirección del scroll
+function bannerScrollHandler() {
+      const currentScroll = window.scrollY; // Actualizamos la posición del scroll cada vez que se ejectuta la función
+
+      // Si el nuevo scroll es mayor que el anterior = scroll hacia abajo
+      if (currentScroll > lastScroll) {
+            bannerTitle.style.transform = `translateX(${currentScroll * -0.5}px)`; // Mueve el título hacia la izquierda
+
+            // Si el nuevo scroll es menor que el anterior = scroll hacia arriba
+      } else if (currentScroll < lastScroll) {
+            bannerTitle.style.transform = `translateX(${-currentScroll * 0.5}px)`; // Mueve el título hacia la derecha
+      }
+
+      lastScroll = currentScroll; // Actualizamos la posición del scroll para la siguiente vez que se ejecute la función
+}
 
 if (bannerTitle) {
-      window.addEventListener('scroll', () => {
-            const currentScroll = window.scrollY;
-
-            if (currentScroll > lastScroll) {
-                  bannerTitle.style.transform = `translateX(${currentScroll * -0.5}px)`; // Mueve el título hacia la izquierda
-            } else if (currentScroll < lastScroll) {
-                  bannerTitle.style.transform = `translateX(${-currentScroll * 0.5}px)`; // Mueve el título hacia la derecha
-            }
-
-            lastScroll = currentScroll;
-      });
+      window.addEventListener('scroll', bannerScrollHandler); // Escuchamos el evento de scroll para mover el título del banner
 }
 
 
 
-// PROYECTOS (individuales) - Interacciones
-const projectsItem = document.querySelectorAll('.gallery picture'); // Solo seleccionamos los picture de la galería, para que el lightbox no se pueda activar con los videos que añadimos
-const lightboxContainer = document.querySelector('.lightbox');
-const lightboxImage = document.querySelector('.lightbox__img');
-const lightboxPicture = document.querySelector('.lightbox__source');
-const lightboxClose = document.querySelector('.lightbox__close');
+/* => Interacción E: Lightbox para las imágenes de los proyectos
+      Descripción: Al hacer click en una imagen de la galería, se muestra un lightbox con la imagen ampliada.
+      Estructura:
+            - Constantes
+            - Función
+            - Evento de click
+*/
+
+const projectsItem = document.querySelectorAll('.gallery picture'); // Solo selecciona los picture de la galería, para que el lightbox no se pueda activar con los videos que añadimos
+const lightboxContainer = document.querySelector('.lightbox'); // Selecciona el contenedor del lightbox
+const lightboxImage = document.querySelector('.lightbox__img'); // Selecciona la imagen del lightbox (en su picture)
+const lightboxPicture = document.querySelector('.lightbox__source'); // Selecciona el source del lightbox (en su picture)
+const lightboxClose = document.querySelector('.lightbox__close'); // Selecciona el botón de cerrar del lightbox
+
+// Función para mostrar el lightbox con la imagen ampliada
+function lightboxHandler(e) {
+      lightboxPicture.srcset = e.target.srcset; // Cambia el source del lightbox al de la imagen que se ha clicado
+      lightboxImage.src = e.target.src; // Cambia la imagen del lightbox a la de la imagen que se ha clicado
+      requestAnimationFrame(() => {
+            lightboxContainer.classList.add('lightbox--active');
+      }); // Añade la clase activo al contenedor del lightbox para mostrarlo
+}
+
 
 if (projectsItem) {
       projectsItem.forEach((item) => {
-            item.addEventListener('click', (e) => {
-                  lightboxPicture.srcset = e.target.srcset;
-                  lightboxImage.src = e.target.src;
-                  setTimeout(() => {
-                        lightboxContainer.classList.add('lightbox--active');
-                  }, 0);
-            });
+            item.addEventListener('click', lightboxHandler); // Por cada imagen, escucha el evento click y ejecuta la función lightboxHandler
       });
 }
+
 if (lightboxClose) {
-      lightboxClose.addEventListener('click', () => {
-            lightboxContainer.classList.remove('lightbox--active');
-      });
+      lightboxClose.addEventListener('click', () => lightboxContainer.classList.remove('lightbox--active')); // Escucha el evento click en el botón de cerrar y ejecuta la función lightboxCloseHandler
 }
+
+
+
+
+
+/* => Interacción F: Mensaje de confirmación de envío de formulario
+      Descripción: Al enviar el formulario de contacto, se muestra un mensaje de confirmación con el nombre del usuario.
+      Estructura:
+            - Constantes
+            - Función
+            - Evento de submit y click
+*/
+
+const form = document.querySelector('.contact__form'); // Selecciona el formulario de contacto
+const confirmationDialog = document.querySelector('.confirmation'); // Selecciona el contenedor del mensaje de confirmación
+const dialogMessage = document.querySelector('.confirmation__message p'); // Selecciona el párrafo del mensaje de confirmación
+const closeDialog = document.querySelector('.confirmation__btn'); // Selecciona el botón de cerrar del mensaje de confirmación
+const nombreInput = document.getElementById('name'); // Selecciona el input del nombre en el formulario
+
+// Función para mostrar el mensaje de confirmación al enviar el formulario
+function formSubmitHandler(e) {
+      e.preventDefault(); // Evita que se envíe el formulario y se recargue la página
+      dialogMessage.textContent = `¡Gracias, ${nombreInput.value}! Nos pondremos en contacto contigo lo antes posible.`; // Cambia el texto del mensaje de confirmación para incluir el nombre del usuario
+      confirmationDialog.classList.add('confirmation--active'); // Añade la clase activo al contenedor del mensaje de confirmación para mostrarlo
+}
+
+// Función para cerrar el mensaje de confirmación
+function closeDialogHandler() {
+      confirmationDialog.classList.remove('confirmation--active'); // Elimina la clase activo del contenedor del mensaje de confirmación para ocultarlo
+}
+
+if (form) {
+      form.addEventListener('submit', formSubmitHandler); // Escucha el evento submit en el formulario y ejecuta la función formSubmitHandler
+      closeDialog.addEventListener('click', closeDialogHandler); // Escucha el evento click en el botón de cerrar y ejecuta la función closeDialogHandler
+}
+
